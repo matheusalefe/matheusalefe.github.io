1ebbd642137f7a1bc31d6e9386a123a16bc9ca6a
Matheus Alefe <matheus.orvati@hotmail.com>
2022-08-18 20:08:00 -0300

    Atualizar a página de impressão

--- js/script.js
+++ js/script.js
@@ -1,11 +1,3 @@
-/*window.alert("ATENÇÃO! O site está passando por atualizações então talvez ocorram erros de responsividade. Obrigado pela compreensão.")*/
-//window.alert("Obrigado por acessar o meu site-currículo! Clicando na foto você terá acesso ao meu currículo para impressão ^^")
-/*function entrar(){
-    var foto = window.document.getElementById('foto-candidato')
-    foto.innerHTML = '<div id="circulo"><a href="docs/Currículo Matheus Alefe.pdf" target="_blank"><p>Clique na foto para baixar o CV!</p></a></div>'
+function imprimirCurriculo(){
+    window.print()
 }
-
-function sair(){
-    var foto = window.document.getElementById('foto-candidato')
-    foto.innerHTML = '<div id="foto-candidato" onmouseenter="entrar()" onmouseout="sair()"><a href="../docs/Currículo Matheus Alefe.pdf" target="_blank"><img src="img/profile-pic.jpg" alt="Matheus Alefe" id="profile-pic" ></a></div>'
-}*/
