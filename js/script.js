28f26db636fc9526c805e0283432c9be90a8a031
Matheus Alefe <matheus.orvati@hotmail.com>
2022-08-18 20:18:46 -0300

    Atualizar funções

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
 function imprimirCurriculo(){
     window.print()
 }
+
+function hoverIn(){
+    var mensagem = window.document.getElementById('mensagem')
+    mensagem.innerText = 'Clique na foto e imprima o currículo!'
+}
+
+function hoverOut(){
+    var mensagem = window.document.getElementById('mensagem')
+    mensagem.innerText = ''
+}
